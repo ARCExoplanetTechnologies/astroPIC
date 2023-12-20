c21bdf0b5b0b359f8c9bcbf2543653542fa4b99b
dsirbu <30481010+dsirbu221@users.noreply.github.com>
2023-12-20 10:24:15 -0800

    added missing "mode_matching" function in simLib

---
+++ simLib/mode_matching.m
@@ -0,0 +1,14 @@
+%% computes \int |Ea* Eb|^2 dA / (|\int Ea dA|^2 + |\int Eb dA|^2) 
+
+function modematch_out = mode_matching(E_a,E_b,pupil)
+
+overlap_EaEb = overlap_integral(E_a,E_b, pupil);
+overlap_EaEa = overlap_integral(abs(E_a),abs(E_a), pupil);
+overlap_EbEb = overlap_integral(abs(E_b),abs(E_b), pupil);
+
+modematch_out = abs(overlap_EaEb).^2 / (overlap_EaEa*overlap_EbEb);
+
+% below is mode matching 
+% function modematch_out = mode_matching(E_a,E_b) 
+% modematch_out = (trapz(trapz(conj(E_a).*E_b))).^2 ./ ...
+%         (trapz(trapz(abs(E_a).^2))*trapz(trapz(abs(E_b).^2)))
